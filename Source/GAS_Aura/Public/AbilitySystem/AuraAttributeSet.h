089c82f3d8edf1066e481918a653c9ec17e7fb38
Larry_Lin <3213573967@qq.com>
2025-11-08 16:01:11 +0800

    Add Health and Mana to AttributeSet

--- Source/GAS_Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/GAS_Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -11,4 +11,27 @@ class GAS_AURA_API UAuraAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
 	
+public:
+	UAuraAttributeSet();
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(BlueprintReadOnly , ReplicatedUsing = Onrep_Health , Category = "Vital Attributes")
+	FGameplayAttributeData Health;
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+
+	UPROPERTY(BlueprintReadOnly , ReplicatedUsing = Onrep_MaxHealth , Category = "Vital Attributes")
+	FGameplayAttributeData MaxHealth;
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
+	UPROPERTY(BlueprintReadOnly , ReplicatedUsing = Onrep_Mana , Category = "Vital Attributes")
+	FGameplayAttributeData Mana;
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+
+	UPROPERTY(BlueprintReadOnly , ReplicatedUsing = Onrep_MaxMana , Category = "Vital Attributes")
+	FGameplayAttributeData MaxMana;
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
 };
