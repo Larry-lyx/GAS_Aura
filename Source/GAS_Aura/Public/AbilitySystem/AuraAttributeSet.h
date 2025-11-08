17ed15271433b582a305670fb165f7ee2fd0ef5e
Larry_Lin <3213573967@qq.com>
2025-11-08 16:16:48 +0800

    Create Attribute Accessors

--- Source/GAS_Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/GAS_Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -4,7 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "AuraAttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 UCLASS()
 class GAS_AURA_API UAuraAttributeSet : public UAttributeSet
@@ -19,19 +26,23 @@ public:
 	FGameplayAttributeData Health;
 	UFUNCTION()
 	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet , Health);
 
 	UPROPERTY(BlueprintReadOnly , ReplicatedUsing = Onrep_MaxHealth , Category = "Vital Attributes")
 	FGameplayAttributeData MaxHealth;
 	UFUNCTION()
 	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet , MaxHealth);
 
 	UPROPERTY(BlueprintReadOnly , ReplicatedUsing = Onrep_Mana , Category = "Vital Attributes")
 	FGameplayAttributeData Mana;
 	UFUNCTION()
 	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet , Mana);
 
 	UPROPERTY(BlueprintReadOnly , ReplicatedUsing = Onrep_MaxMana , Category = "Vital Attributes")
 	FGameplayAttributeData MaxMana;
 	UFUNCTION()
 	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet , MaxMana);
 };
