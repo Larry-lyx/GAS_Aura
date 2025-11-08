acbff92d800c663305b72cdd06e1f6ccc599fe52
Larry_Lin <3213573967@qq.com>
2025-11-08 19:56:57 +0800

    Aura HUD

---
+++ Source/GAS_Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,14 @@
+// Copyright GAS Aura Course
+
+
+#include "UI/HUD/AuraHUD.h"
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld() , OverlayWidgetClass);
+	Widget->AddToViewport();
+}
