3d7c327239cbd795a9e9e979892a2527af0206b6
Larry_Lin <3213573967@qq.com>
2025-11-09 19:43:45 +0800

    Instant Gameplay Effects

--- Source/GAS_Aura/Public/Actor/AuraEffectActor.h
+++ Source/GAS_Aura/Public/Actor/AuraEffectActor.h
@@ -6,7 +6,8 @@
 #include "GameFramework/Actor.h"
 #include "AuraEffectActor.generated.h"
 
-class USphereComponent;
+
+class UGameplayEffect;
 
 UCLASS()
 class GAS_AURA_API AAuraEffectActor : public AActor
@@ -15,18 +16,13 @@ class GAS_AURA_API AAuraEffectActor : public AActor
 	
 public:	
 	AAuraEffectActor();
-
-	UFUNCTION()
-	virtual void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
-	UFUNCTION()
-	virtual void EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 	
 protected:
 	virtual void BeginPlay() override;
-	
-private:
-	UPROPERTY(VisibleAnywhere)
-	UStaticMeshComponent* Mesh;
-	UPROPERTY(VisibleAnywhere)
-	USphereComponent* Sphere;
+
+	UFUNCTION(BlueprintCallable)
+	void ApplyEffectToTarget(AActor* TargetActor , TSubclassOf<UGameplayEffect> GameplayEffectClass);
+
+	UPROPERTY(EditAnywhere , BlueprintReadOnly , Category = "Applied Effects")
+	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
 };
