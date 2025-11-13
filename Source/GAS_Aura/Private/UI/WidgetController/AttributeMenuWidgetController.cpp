c54243f4479e92e7a3cfe18cb7540e94460340db
Larry_Lin <3213573967@qq.com>
2025-11-13 15:29:47 +0800

    AbilitySystemLibrary and Attribute Menu Widget Controller

---
+++ Source/GAS_Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright GAS Aura Course
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+	Super::BindCallbacksToDependencies();
+}
+
+void UAttributeMenuWidgetController::BroadcastInitValues()
+{
+	Super::BroadcastInitValues();
+}
