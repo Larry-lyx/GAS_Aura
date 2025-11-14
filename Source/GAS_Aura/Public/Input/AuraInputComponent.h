cbdd5fbe4b1bba51085f445f34f1493e16909182
Larry_Lin <3213573967@qq.com>
2025-11-14 09:17:40 +0800

    Aura Input Component

--- Source/GAS_Aura/Public/Input/AuraInputComponent.h
+++ Source/GAS_Aura/Public/Input/AuraInputComponent.h
@@ -3,15 +3,42 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AuraConfigInput.h"
 #include "EnhancedInputComponent.h"
 #include "AuraInputComponent.generated.h"
 
-/**
- * 
- */
+
 UCLASS()
 class GAS_AURA_API UAuraInputComponent : public UEnhancedInputComponent
 {
 	GENERATED_BODY()
+
+public:
+	template<class UserClass , typename PressedFuncType , typename ReleasedFuncType , typename HeldFuncType>
+	void BindAbilityActions(const UAuraConfigInput& InputConfig , UserClass* Object , PressedFuncType PressedFunc , ReleasedFuncType ReleasedFunc , HeldFuncType HeldFunc);
 	
 };
+
+template <class UserClass, typename PressedFuncType, typename ReleasedFuncType, typename HeldFuncType>
+void UAuraInputComponent::BindAbilityActions(const UAuraConfigInput& InputConfig, UserClass* Object,
+	PressedFuncType PressedFunc, ReleasedFuncType ReleasedFunc, HeldFuncType HeldFunc)
+{
+	for (const FAuraInputAction& Action : InputConfig.AbilityInputActions)
+	{
+		if (Action.InputAction && Action.InputTag.IsValid())
+		{
+			if (PressedFunc)
+			{
+				BindAction(Action.InputAction , ETriggerEvent::Started , Object , PressedFunc , Action.InputTag);
+			}
+			if (ReleasedFunc)
+			{
+				BindAction(Action.InputAction , ETriggerEvent::Completed , Object , ReleasedFunc , Action.InputTag);
+			}
+			if (HeldFunc)
+			{
+				BindAction(Action.InputAction , ETriggerEvent::Triggered , Object , HeldFunc , Action.InputTag);
+			}
+		}
+	}
+}
