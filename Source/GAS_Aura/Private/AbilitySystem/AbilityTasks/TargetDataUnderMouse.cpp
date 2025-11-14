cb2ae4faa4e156ebfdb5626577bfa56790e45290
Larry_Lin <3213573967@qq.com>
2025-11-14 20:29:10 +0800

    Custom Ability Tasks

---
+++ Source/GAS_Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
@@ -0,0 +1,18 @@
+// Copyright GAS Aura Course
+
+
+#include "AbilitySystem/AbilityTasks/TargetDataUnderMouse.h"
+
+UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility)
+{
+	UTargetDataUnderMouse* MyObj = NewAbilityTask<UTargetDataUnderMouse>(OwningAbility);
+	return MyObj;
+}
+
+void UTargetDataUnderMouse::Activate()
+{
+	APlayerController* PC = Ability->GetCurrentActorInfo()->PlayerController.Get();
+	FHitResult CursorHitResult;
+	PC->GetHitResultUnderCursor(ECC_Visibility , false , CursorHitResult);
+	ValidData.Broadcast(CursorHitResult.Location);
+}
