0d9757142a10cae471d34c3472ceacdc8cbc418e
Larry_Lin <3213573967@qq.com>
2025-11-14 20:01:40 +0800

    Spawn FireBolt from Event

--- Source/GAS_Aura/Public/AbilitySystem/Abilities/AuraProjectileSpell.h
+++ Source/GAS_Aura/Public/AbilitySystem/Abilities/AuraProjectileSpell.h
@@ -16,6 +16,9 @@ class GAS_AURA_API UAuraProjectileSpell : public UAuraGameplayAbility
 protected:
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 
+	UFUNCTION(BlueprintCallable , Category = "Projectile")
+	void SpawnProjectile();
+	
 	UPROPERTY(EditDefaultsOnly , BlueprintReadOnly)
 	TSubclassOf<AAuraProjectile> ProjectileClass;
 };
