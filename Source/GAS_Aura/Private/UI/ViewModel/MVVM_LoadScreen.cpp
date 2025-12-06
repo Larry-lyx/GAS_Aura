4abfabfcdedc53e70714f34cb6530dd63ed57886
Larry_Lin <3213573967@qq.com>
2025-12-06 16:05:41 +0800

    Save the Player Tag

--- Source/GAS_Aura/Private/UI/ViewModel/MVVM_LoadScreen.cpp
+++ Source/GAS_Aura/Private/UI/ViewModel/MVVM_LoadScreen.cpp
@@ -40,6 +40,7 @@ void UMVVM_LoadScreen::NewSlotButtonPressed(int32 Slot, const FString& EnteredNa
 	LoadSlots[Slot]->SetMapName(AuraGameMode->DefaultMapName);
 	LoadSlots[Slot]->SetPlayerName(EnteredName);
 	LoadSlots[Slot]->SlotStatus = Taken;
+	LoadSlots[Slot]->PlayerStartTag = AuraGameMode->DefaultPlayerStartTag;
 	
 	AuraGameMode->SaveSlotData(LoadSlots[Slot] , Slot);
 	LoadSlots[Slot]->InitializeSlot();
@@ -86,6 +87,9 @@ void UMVVM_LoadScreen::DeleteButtonPressed()
 void UMVVM_LoadScreen::PlayButtonPressed()
 {
 	AAuraGameModeBase* AuraGameMode = Cast<AAuraGameModeBase>(UGameplayStatics::GetGameMode(this));
+	UAuraGameInstance* AuraGameInstance = Cast<UAuraGameInstance>(AuraGameMode->GetGameInstance());
+
+	AuraGameInstance->PlayerStartTag = SelectedSlot->PlayerStartTag;
 
 	if (IsValid(SelectedSlot))
 	{
@@ -112,6 +116,8 @@ void UMVVM_LoadScreen::LoadData()
 		LoadSlot.Value->SlotStatus = SaveSlotStatus;
 		LoadSlot.Value->SetPlayerName(PlayerName);
 		LoadSlot.Value->InitializeSlot();
+		
 		LoadSlot.Value->SetMapName(SaveObject->MapName);
+		LoadSlot.Value->PlayerStartTag = SaveObject->PlayerStartTag;
 	}
 }
--- Source/GAS_Aura/Public/UI/ViewModel/MVVM_LoadSlot.h
+++ Source/GAS_Aura/Public/UI/ViewModel/MVVM_LoadSlot.h
@@ -33,6 +33,9 @@ public:
 
 	UPROPERTY()
 	TEnumAsByte<ESaveSlotStatus> SlotStatus;
+
+	UPROPERTY()
+	FName PlayerStartTag;
 	
 	void SetLoadSlotName(FString InLoadSlotName);
 	void SetPlayerName(FString InPlayerName);
